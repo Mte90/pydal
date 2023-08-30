197cfedf00e52f0ec11d661e16ba20f274e1095b
Daniele Scasciafratte <mte90net@gmail.com>
2023-08-30 16:15:05 +0200

    feat(readme): improved

--- pydal.py
+++ pydal.py
@@ -1,55 +1,66 @@
 #!/usr/bin/python3
-import configparser, argparse, os, subprocess
+import configparser
+import argparse
+import os
+import sys
+import subprocess
 from evdev import InputDevice, categorize, ecodes, list_devices
 
-parser = argparse.ArgumentParser(description='Pydal is a simple tool to change the behaviour of a bekyboard with scripts on Linux')
-parser.add_argument('-devices', help='The devices list', nargs='?', action='store', const='1')
-parser.add_argument('-run', help='Launch Pydal', nargs='?', action='store', const='1')
-parser.add_argument('--config', help='Config file for Pydal', nargs='?', action='store', const='')
-parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+parser = argparse.ArgumentParser(
+    description="Pydal is a tiny script to assign a script to a button of a specific keyboard"
+)
+parser.add_argument("-devices", help="The devices list", nargs="?", action="store", const="1")
+parser.add_argument("-run", help="Launch Pydal", nargs="?", action="store", const="1")
+parser.add_argument("--config", help="Config file for Pydal", nargs="?", action="store", const="")
+parser.add_argument("--version", action="version", version="%(prog)s 1.0")
 args = parser.parse_args()
 
-if args.devices is not None or args.run is not None:
-    print('Pydal started...')
-    # Load configuration
-    if args.config is not None:
-        if not os.path.exists(args.config):
-            print('Config file not found on ' + args.config)
-            exit()
-        print('  Config loaded!')
-        config = configparser.RawConfigParser()
-        config.read_file(open(args.config))
-        hotkeys = {}
-        key = 0
-
-        for section in config.sections():
-            if 'key' in config[section]:
-                key += 1
-                hotkeys[config.get(section, 'key')] = [config.get(section, 'status'), config.get(section, 'run')]
-        print('Find ' + str(key) + ' hotkeys to configure...')
-
-    devices = [InputDevice(fn) for fn in reversed(list_devices())]
-    print('Looking for the devices...')
-    gotit = False
-    for device in devices:
-        if args.devices:
-            print(device)
-        if args.run:
-            if device.name == config.get('keyboard', 'name'):
-                gotit = True
-                print('  Device found!')
-                dev = InputDevice(device.path)
-                dev.grab()
-                for event in dev.read_loop():
-                    if event.type == ecodes.EV_KEY:
-                        press = categorize(event)
-                        button = press.keycode
-                        button = button.replace('KEY_', '')
-                        if button in hotkeys:
-                            if hotkeys[button][0] == 'keydown' and press.key_down or hotkeys[button][0] == 'keyup' and press.key_up:
-                                print('Executing script for ' + button + ' on ' + hotkeys[button][0])
-                                subprocess.Popen(hotkeys[button][1], shell = True)
-    if gotit is False and args.config is not None:
-        print('  Device ' + config.get('keyboard', 'name') + ' not found :-(')
-else:
+if args.devices is None and args.run is None:
     parser.print_help()
+    sys.exit()
+
+print("Pydal started...")
+
+# Load configuration
+if args.config is not None:
+    if not os.path.exists(args.config):
+        print("Config file not found on " + args.config)
+        exit()
+    print("  Config loaded!")
+    config = configparser.RawConfigParser()
+    config.read_file(open(args.config))
+    hotkeys = {}
+    key = 0
+
+    for section in config.sections():
+        if "key" in config[section]:
+            key += 1
+            hotkeys[config.get(section, "key")] = [config.get(section, "status"), config.get(section, "run")]
+    print("Find " + str(key) + " hotkeys to configure...")
+
+devices = [InputDevice(fn) for fn in reversed(list_devices())]
+print("Looking for the devices...")
+gotit = False
+for device in devices:
+    # Print devices
+    if args.devices:
+        print(device)
+
+    # Read inputs and replace macros
+    if args.run:
+        if device.name == config.get("keyboard", "name"):
+            gotit = True
+            print("  Device found!")
+            dev = InputDevice(device.path)
+            dev.grab()
+            for event in dev.read_loop():
+                if event.type == ecodes.EV_KEY:
+                    press = categorize(event)
+                    button = press.keycode.replace("KEY_", "")
+                    if button in hotkeys:
+                        if hotkeys[button][0] == "keyup" and press.key_up:
+                            print("Executing script for " + button + " on " + hotkeys[button][0])
+                            subprocess.Popen(hotkeys[button][1], shell=True)
+
+if gotit is False and args.config is not None:
+    print("  Device " + config.get("keyboard", "name") + " not found :-(")
--- readme.md
+++ readme.md
@@ -1,18 +1,12 @@
-# What?!?
-[![License](https://img.shields.io/badge/License-BSD%20v3-blue.svg)](https://spdx.org/licenses/BSD-3-Clause)   
-[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2FMte90%2Fpydal.svg?type=shield)](https://app.fossa.io/projects/git%2Bgithub.com%2FMte90%2Fpydal?ref=badge_shield)
+# Pydal
+[![License](https://img.shields.io/badge/License-BSD%20v3-blue.svg)](https://spdx.org/licenses/BSD-3-Clause)
 
+A simple way to create your macros, with a config file to execute cusom script/program and target a specific keyboard.
 
 ![](https://daniele.tech/wp-content/uploads/2018/05/footswitch-300x300.jpg)
 
-As you can see in photo, in my case I have a foot switch and on Linux there is no official support by no one producer to configure it.  
-Should be cool that every button execute a specific script (that do magic stuff of course) and not only a classic press event that print only `1`?  
-With Python 3 and few permissions enabled this solution works on all the Linux distro.  
-
-## Why
-
-In my case I am using this footswitch to move the cursor between my 3 monitor to get the focus on the software opened at fullscreen that I usually have.  
-THis is a very poor example but I want to get before experience (or remember that I have a pedal) and in the future use it for more advanced things.
+As you can see in photo, in my case I have a foot switch and in this way I am able to use it to run scripts that move my mouse cursor in one of the 3 monitor I have.
+This tiny Python script, parse the config file and map to the specific keyboard configured a script to execute on the various buttons.
 
 # Configure
 
@@ -35,7 +29,7 @@ optional arguments:
 ## First step
 
 Run `pydal.py -devices` to get a list of all the HID devices connected to your computer.  
-Save the name of the keyboard that you want script somewhere.
+Save the name of the keyboard that you need to for your config file.
 
 ## Second step
 
@@ -60,8 +54,3 @@ usermod -a -G plugdev mte90
 ```
 
 Add the udev rules to your system `99-pydal.rules` on `/etc/udev/rules.d/`.
-
-
-
-## License
-[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2FMte90%2Fpydal.svg?type=large)](https://app.fossa.io/projects/git%2Bgithub.com%2FMte90%2Fpydal?ref=badge_large)
